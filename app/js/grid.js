f4664b016706d4323ac98fe518cfb2881b4c4701
Colin PUY <colin.puy@bonitasoft.com>
2014-05-06 12:14:17 +0200

    refactor gridService

--- app/js/grid.js
+++ app/js/grid.js
@@ -6,18 +6,28 @@
 angular.module('service.grid', [])
     .factory('gridService', function () {
 
-        var create = function (nbColumn, nbRows) {
-            var cells = [], i = 0, j = 0;
-            for (i = 0; i < nbRows; i++) {
-                cells[i] = [];
-                for (j = 0; j < nbColumn; j++) {
-                    cells[i].push({alive: false});
-                }
-            }
-            return cells;
+        var Cell = function () {
+            return { alive: false };
         };
 
-        var toggleLife = function(cell) {
+        var create = function (nbColumn, nbRows) {
+            var cells = [], i = 0;
+
+            for (i = 0; i < nbRows; i++) {
+                cells.push(createColumn());
+            }
+            return cells;
+
+            function createColumn() {
+                var column = [], j = 0;
+                for (j = 0; j < nbColumn; j++) {
+                    column.push(new Cell());
+                }
+                return column;
+            };
+        };
+
+        var toggleLife = function (cell) {
             cell.alive = !cell.alive;
         };
 

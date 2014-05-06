cb1646fcea3a5f4c3fa3563832323269e1bf4668
Colin PUY <colin.puy@bonitasoft.com>
2014-05-06 14:01:53 +0200

    refactor grid.js

--- app/js/grid.js
+++ app/js/grid.js
@@ -10,9 +10,25 @@ angular.module('service.grid', [])
             return { alive: false };
         };
 
-        var Grid = function(nbColumns, nbRows) {
+        var Grid = function (nbColumns, nbRows) {
             this.nbColumns = nbColumns || 12;
             this.nbRows = nbRows || 12;
+
+            this.countAliveNeighbours = function (columnIdx, rowIdx) {
+                var count = 0;
+                for (var i = columnIdx - 1; i <= columnIdx + 1; i++) {
+                    for (var j = rowIdx - 1; j <= rowIdx + 1; j++) {
+                        if (!(i == columnIdx && j == rowIdx) && this.isAlive(i, j)) {
+                            count += 1;
+                        }
+                    }
+                }
+                return count;
+            };
+
+            this.isAlive = function (columnIdx, rowIdx) {
+                return this[columnIdx] && this[columnIdx][rowIdx] && this[columnIdx][rowIdx].alive;
+            };
 
             for (var i = 0; i < nbRows; i++) {
                 this[i] = createColumn();
@@ -37,35 +53,23 @@ angular.module('service.grid', [])
 
         var nextGeneration = function (grid) {
             var count = 0;
-            var newGrid = new Grid(grid.nbColumns, grid.nbColumns);
+            var nextGenGrid = new Grid(grid.nbColumns, grid.nbColumns);
 
             for (var i = 0; i < grid.nbRows; i++) {
                 for (var j = 0; j < grid.nbColumns; j++) {
 
-                    count = countAliveNeighbours(grid, i, j);
+                    count = grid.countAliveNeighbours(i, j);
                     if (count === 3) {
-                        newGrid[i][j].alive = true;
+                        nextGenGrid[i][j].alive = true;
                     }
 
                     else if (grid[i][j].alive && count === 2) {
-                        newGrid[i][j].alive = true;
+                        nextGenGrid[i][j].alive = true;
                     }
                 }
             }
-            return newGrid;
+            return nextGenGrid;
         };
-
-        var countAliveNeighbours = function(grid, columnIdx, rowIdx) {
-            var count = 0;
-            for (var i = columnIdx - 1; i <= columnIdx + 1; i++) {
-                for (var j = rowIdx - 1; j <= rowIdx + 1; j++) {
-                    if (!(i == columnIdx && j == rowIdx) && grid[i] && grid[i][j] && grid[i][j].alive) {
-                        count += 1;
-                    }
-                }
-            }
-            return count;
-        }
 
         return {
             create: createGrid,

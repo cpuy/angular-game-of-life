afb522067939e6ddec1fd35c7a078bafc8e13360
Colin PUY <colin.puy@bonitasoft.com>
2014-05-07 12:43:18 +0200

    use service in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,92 +1,11 @@
 'use strict';
 
-var Grid = function () {
-    this.nbColumn = 12;
-    this.nbRows = 12;
-    this.cells = buildCells(this.nbColumn, this.nbRows);
-
-    function buildCells(nbColumn, nbRows) {
-        var cells = [], column = [];
-        for (var i = 0; i < nbRows; i++) {
-            var column = [];
-            for (var j = 0; j < nbColumn; j++) {
-                column[j] = {alive: false};
-            }
-            cells[i] = column;
-        }
-        return cells;
-    };
-}
-
-Grid.prototype.isAlive = function (column, row) {
-    return this.cells[column][row].alive;
-};
-
-Grid.prototype.giveLife = function (column, row) {
-    this.cells[column][row].alive = true;
-};
-
-Grid.prototype.nextGeneration = function () {
-    var that = this;
-    var newGrid = new Grid();
-    var cell;
-    for (var i = 0; i < this.nbRows; i++) {
-        for (var j = 0; j < this.nbColumn; j++) {
-            var count = countAliveNeighbors(i, j);
-            if (this.cells[i][j].alive) {
-
-                if (count === 2 || count === 3) {
-                    newGrid.cells[i][j].alive = true;
-                }
-            } else {
-                if (count === 3) {
-                    newGrid.cells[i][j].alive = true;
-                }
-            }
-        }
-    }
-    return newGrid;
-
-    function countAliveNeighbors(i, j) {
-        var count = 0;
-        if (that.cells[i - 1]) {
-            if (that.cells[i - 1][j] && that.cells[i - 1][j].alive) {
-                count += 1;
-            }
-            if (that.cells[i - 1][j - 1] && that.cells[i - 1][j - 1].alive) {
-                count += 1;
-            }
-            if (that.cells[i - 1][j + 1] && that.cells[i - 1][j + 1].alive) {
-                count += 1;
-            }
-        }
-        if (that.cells[i + 1]) {
-            if (that.cells[i + 1][j] && that.cells[i + 1][j].alive) {
-                count += 1;
-            }
-            if (that.cells[i + 1][j + 1] && that.cells[i + 1][j + 1].alive) {
-                count += 1;
-            }
-            if (that.cells[i + 1][j - 1] && that.cells[i + 1][j - 1].alive) {
-                count += 1;
-            }
-        }
-        if (that.cells[i][j + 1] && that.cells[i][j + 1].alive) {
-            count += 1;
-        }
-        if (that.cells[i][j - 1] && that.cells[i][j - 1].alive) {
-            count += 1;
-        }
-        return count;
-    }
-};
-
-angular.module('gameOfLife.controllers', [])
-    .controller('GridCtrl', [ '$scope', '$interval', 'grid', function ($scope, $interval, grid) {
-        $scope.grid = grid;
+angular.module('gameOfLife.controllers', ['service.grid'])
+    .controller('GridCtrl', [ '$scope', '$interval', 'gridService', function ($scope, $interval, gridService) {
+        $scope.grid = gridService.create(12, 12);
 
         $scope.toggleLife = function (cell) {
-            cell.alive = !cell.alive;
+            gridService.toggleLife(cell);
         };
 
         $scope.nextGeneration = function () {
@@ -94,7 +13,6 @@ angular.module('gameOfLife.controllers', [])
         };
 
         function computeNextGeneration() {
-            $scope.grid = $scope.grid.nextGeneration();
+            $scope.grid = gridService.nextGeneration($scope.grid);
         };
-    }])
-    .service('grid', Grid);
+    }]);
